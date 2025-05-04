777c78c5c0f23425ce7dc2034207d9013280b964
brunolucero19 <brunolucero2419@gmail.com>
2025-05-04 14:37:45 -0300

    feat: integrate Tailwind CSS into the project
    - Added Tailwind CSS and its Vite plugin to dependencies in package.json
    - Updated App component to use Tailwind CSS for styling the header
    - Configured Vite to use Tailwind CSS plugin
    - Created a new styles.css file to import Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ComponenteEjemplo from './components/ComponenteEjemplo'
 function App() {
   return (
     <>
-      <h1>Stockify Front</h1>
+      <h1 className='text-red-500'>Stockify Front</h1>
       <ComponenteEjemplo />
     </>
   )
