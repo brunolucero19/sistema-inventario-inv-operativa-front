6328d1740963b8052d2f03cc8564aa35a38110a6
brunolucero19 <brunolucero2419@gmail.com>
2025-05-04 13:14:42 -0300

    Add API connection in ComponenteEjemplo component

--- src/components/ComponenteEjemplo.jsx
+++ src/components/ComponenteEjemplo.jsx
@@ -1,5 +1,19 @@
 const ComponenteEjemplo = () => {
-  return <h2>Este es un componente de ejemplo</h2>
+  const url = import.meta.env.VITE_BACKEND_URL
+
+  const conectarAPI = async () => {
+    const response = await fetch(url)
+    const { message } = await response.json()
+    console.log(message)
+  }
+
+  conectarAPI()
+
+  return (
+    <>
+      <h2>Este es un componente de ejemplo</h2>
+    </>
+  )
 }
 
 export default ComponenteEjemplo
