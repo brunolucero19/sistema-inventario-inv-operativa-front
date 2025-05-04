0e449fd4d1fab6dcbb8889da2739aea7661ce011
brunolucero19 <brunolucero2419@gmail.com>
2025-05-04 12:19:04 -0300

    Add ComponenteEjemplo component and update App to include it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import ComponenteEjemplo from './components/ComponenteEjemplo'
+
 function App() {
-  return <h1>Stockify Front</h1>
+  return (
+    <>
+      <h1>Stockify Front</h1>
+      <ComponenteEjemplo />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/ComponenteEjemplo.jsx
@@ -0,0 +1,5 @@
+const ComponenteEjemplo = () => {
+  return <h2>Este es un componente de ejemplo</h2>
+}
+
+export default ComponenteEjemplo
