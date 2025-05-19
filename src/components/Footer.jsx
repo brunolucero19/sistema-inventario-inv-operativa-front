bbf261617c4a418ac646d0592d6c2ac918067737
ElPacho21 <rechefacundo1@gmail.com>
2025-05-19 17:50:47 -0300

    Fix logout button, add transparent logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import LogOut from '../../public/icons/LogOut'
 const Footer = () => {
   return (
     <footer className='relative flex items-center h-16 bg-gray-800 p-4 justify-center'>
-        <div className='flex items-center gap-2 absolute left-4 border border-red-500 text-red-500 px-3 py-1 rounded hover:bg-red-500 hover:text-white transition cursor-pointer'>
-        <LogOut />
-        <Link to='/'>
+      <Link to='/'>
+        <div className='flex items-center gap-2 border border-red-500 text-red-500 px-3 py-1 rounded hover:bg-red-500 hover:text-white transition cursor-pointer'>
+          <LogOut />
           Salir
-        </Link>
-      </div>
-  <h3 className="text-center w-full">Copyright ©️ 2025. Todos los derechos reservados</h3>
-</footer>
+        </div>
+      </Link>
+      <h3 className="text-center w-full">Copyright ©️ 2025. Todos los derechos reservados</h3>
+    </footer>
   )
 }
 export default Footer
