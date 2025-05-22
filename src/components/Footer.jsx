51007ef707748a8bb3056648ba3e6049940c5bf9
Valentino Isgro <valentinoisgro@gmail.com>
2025-05-21 22:57:37 -0300

    change: css footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import LogOut from '../../public/icons/LogOut'
 
 const Footer = () => {
   return (
-    <footer className='relative flex items-center h-16 bg-gray-800 p-4 justify-center'>
+    <footer className='relative flex items-center h-10 bg-gray-800 p-4 justify-center'>
       <Link to='/'>
         <div className='flex items-center gap-2 border border-red-500 text-red-500 px-3 py-1 rounded hover:bg-red-500 hover:text-white transition cursor-pointer'>
           <LogOut />
