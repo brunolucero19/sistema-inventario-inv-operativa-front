b3d56d326d81ab96331601ff61352b204fdbb813
Nicolashh <nicoo2003@gmail.com>
2025-06-05 12:31:35 -0300

    add ToastContainer in Modal component

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,6 +1,20 @@
+import { ToastContainer } from "react-toastify"
+
 const Modal = ({ children, modalRef }) => {
   return (
-    <dialog ref={modalRef} className='modal'>
+    <dialog ref={modalRef} className='modal' style={{zIndex: 1}}>
+      <ToastContainer
+        position='top-center'
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
       <div className='modal-box bg-gray-800'>
         <form method='dialog'>
           {/* if there is a button in form, it will close the modal */}
