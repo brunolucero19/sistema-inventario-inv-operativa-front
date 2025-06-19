f124d2c614211a1257b64609ecc853fcd762c487
Nicolashh <nicoo2003@gmail.com>
2025-06-19 12:07:45 -0300

    refactor: add Edit button in OrdenCard component

--- src/components/ordenes/OrdenCard.jsx
+++ src/components/ordenes/OrdenCard.jsx
@@ -1,12 +1,19 @@
-export const OrdenCard = ({ orden, onClick}) => {
+import { Edit } from "lucide-react"
+
+export const OrdenCard = ({ orden, onClick }) => {
 
     return (
         <div
             key={orden.id_orden_compra}
-            onClick={() => onClick?.(orden)}
-            className="bg-white rounded-xl cursor-pointer shadow-md border border-gray-200 p-6 transition-transform hover:scale-105 hover:shadow-lg"
+            className="bg-white rounded-xl shadow-md border border-gray-200 p-6 transition-transform hover:scale-105 hover:shadow-lg"
         >
-            <h3 className="text-xl font-bold text-gray-800 mb-2">Orden #{orden.id_orden_compra}</h3>
+            <div className="flex justify-between items-center">
+                <h3 className="text-xl font-bold text-gray-800 mb-2">Orden #{orden.id_orden_compra}</h3>
+                {onClick && <button onClick={() => onClick(orden)} className="cursor-pointer">
+                    <Edit className='w-5 h-5 text-blue-600' />
+                </button>}
+            </div>
+
             <p className="text-gray-700"><strong>Proveedor:</strong> {orden.proveedorArticulo.proveedor.apellido} {orden.proveedorArticulo.proveedor.nombre}</p>
 
             <p className="text-gray-700"><strong>Artículo:</strong> {orden.proveedorArticulo.articulo.descripcion}</p>
