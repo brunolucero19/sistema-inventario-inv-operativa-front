4a31d60fc681a6f97c31183e9b1be73afebe904c
brunolucero19 <brunolucero2419@gmail.com>
2025-06-22 10:34:26 -0300

    refactor: clean up ModificarOrdenCompra component and improve OrdenCard layout

--- src/components/ordenes/ModificarOrdenCompra.jsx
+++ src/components/ordenes/ModificarOrdenCompra.jsx
@@ -2,125 +2,150 @@ import { useState, useEffect } from 'react'
 import ButtonLayout from '../ui/ButtonLayout'
 
 export const ModificarOrdenCompra = ({ orden, onSubmit, onCancel }) => {
-    const [localOrden, setLocalOrden] = useState(null)
+  const [localOrden, setLocalOrden] = useState(null)
 
-    useEffect(() => {
-        if (orden) {
-            setLocalOrden(orden)
-        }
-    }, [orden])
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if (!localOrden) return null
-
-        onSubmit(localOrden)
+  useEffect(() => {
+    if (orden) {
+      setLocalOrden(orden)
     }
-    const handleCancel = () => {
-        onCancel()
-        setLocalOrden(null)
-    }
+  }, [orden])
 
-    const Options = ({ estadoId }) => {
-
-        switch (estadoId) {
-            case 1:
-                return (
-                    <>
-                        <option value='1' className='bg-gray-400 text-black'>Pendiente</option>
-                        <option value='2' className='bg-gray-400 text-black'>Cancelada</option>
-                        <option value='3' className='bg-gray-400 text-black'>Enviada</option>
-                    </>
-                )
-            case 2:
-                return (
-                    <>
-                        <option value='2' className='bg-gray-400 text-black'>Cancelada</option>
-                    </>
-                )
-            case 3:
-                return (
-                    <>
-                        <option value='3' className='bg-gray-400 text-black'>Enviada</option>
-                        <option value='4' className='bg-gray-400 text-black'>Finalizada</option>
-                    </>
-                )
-            case 4:
-                return (
-                    <>
-                        <option value='4' className='bg-gray-400 text-black'>Finalizada</option>
-                    </>
-                )
-            default:
-                return null
-        }
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
     if (!localOrden) return null
-    return (
-        <>
-            <h1 className='text-center font-bold uppercase'>Modificar Orden</h1>
 
-            <form onSubmit={handleSubmit} className='flex flex-col gap-2 my-4'>
+    onSubmit(localOrden)
+  }
+  const handleCancel = () => {
+    onCancel()
+    setLocalOrden(null)
+  }
 
-                <p><strong>ID:</strong> {localOrden.id_orden_compra}</p>
-                <p><strong>Proveedor:</strong> {localOrden.proveedorArticulo.proveedor.apellido} {localOrden.proveedorArticulo.proveedor.nombre}</p>
-                <p><strong>Artículo:</strong> {localOrden.proveedorArticulo.articulo.descripcion}</p>
-                <p><strong>Fecha estimada:</strong> {new Date(localOrden.fecha_estimada_recepcion).toLocaleDateString()}</p>
+  const Options = ({ estadoId }) => {
+    switch (estadoId) {
+      case 1:
+        return (
+          <>
+            <option value='1' className='bg-gray-400 text-black'>
+              Pendiente
+            </option>
+            <option value='2' className='bg-gray-400 text-black'>
+              Cancelada
+            </option>
+            <option value='3' className='bg-gray-400 text-black'>
+              Enviada
+            </option>
+          </>
+        )
+      case 2:
+        return (
+          <>
+            <option value='2' className='bg-gray-400 text-black'>
+              Cancelada
+            </option>
+          </>
+        )
+      case 3:
+        return (
+          <>
+            <option value='3' className='bg-gray-400 text-black'>
+              Enviada
+            </option>
+            <option value='4' className='bg-gray-400 text-black'>
+              Finalizada
+            </option>
+          </>
+        )
+      case 4:
+        return (
+          <>
+            <option value='4' className='bg-gray-400 text-black'>
+              Finalizada
+            </option>
+          </>
+        )
+      default:
+        return null
+    }
+  }
 
-                <div className='flex items-center justify-between gap-2'>
+  if (!localOrden) return null
+  return (
+    <>
+      <h1 className='text-center font-bold uppercase'>Modificar Orden</h1>
 
-                </div>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-2 my-4'>
+        <p>
+          <strong>ID:</strong> {localOrden.id_orden_compra}
+        </p>
+        <p>
+          <strong>Proveedor:</strong>{' '}
+          {localOrden.proveedorArticulo.proveedor.apellido}{' '}
+          {localOrden.proveedorArticulo.proveedor.nombre}
+        </p>
+        <p>
+          <strong>Artículo:</strong>{' '}
+          {localOrden.proveedorArticulo.articulo.descripcion}
+        </p>
+        <p>
+          <strong>Fecha estimada de recepción:</strong>{' '}
+          {new Date(localOrden.fecha_estimada_recepcion).toLocaleDateString()}
+        </p>
 
-                <div className='grid grid-cols-[auto_2fr] items-center gap-3'>
+        <div className='flex items-center justify-between gap-2'></div>
 
-                    <label htmlFor='cantidad'>Cantidad:</label>
-                    <input
-                        type='number'
-                        id='cantidad'
-                        disabled={orden?.id_estado_orden_compra !== 1}
-                        className='border border-gray-300 rounded-lg p-2 w-30 disabled:opacity-50'
-                        value={+localOrden.cantidad}
-                        onChange={(e) =>
-                            setLocalOrden({ ...localOrden, cantidad: +e.target.value })
-                        }
-                        min={1}
-                    />
+        <div className='grid grid-cols-[auto_2fr] items-center gap-3'>
+          <label htmlFor='cantidad'>Cantidad:</label>
+          <input
+            type='number'
+            id='cantidad'
+            disabled={orden?.id_estado_orden_compra !== 1}
+            className='border border-gray-300 rounded-lg p-2 w-30 disabled:opacity-50'
+            value={+localOrden.cantidad}
+            onChange={(e) =>
+              setLocalOrden({ ...localOrden, cantidad: +e.target.value })
+            }
+            min={1}
+          />
 
+          <label htmlFor='estado'>Estado: </label>
+          <select
+            id='estado'
+            className='border border-gray-300 bg-gray-800 rounded-md p-2 text-white w-30 disabled:opacity-50'
+            disabled={
+              orden?.id_estado_orden_compra === 2 ||
+              orden?.id_estado_orden_compra === 4
+            }
+            value={localOrden.id_estado_orden_compra}
+            onChange={(e) =>
+              setLocalOrden({
+                ...localOrden,
+                id_estado_orden_compra: +e.target.value,
+              })
+            }
+          >
+            <Options estadoId={+orden?.id_estado_orden_compra} />
+          </select>
+        </div>
 
-                    <label htmlFor='estado'>Estado: </label>
-                    <select
-                        id='estado'
-                        className='border border-gray-300 bg-gray-800 rounded-md p-2 text-white w-30 disabled:opacity-50'
-                        disabled={orden?.id_estado_orden_compra === 2 || orden?.id_estado_orden_compra === 4}
-                        value={localOrden.id_estado_orden_compra}
-                        onChange={(e) =>
-                            setLocalOrden({
-                                ...localOrden,
-                                id_estado_orden_compra: +e.target.value
-                            })
-                        }
-                    >
-                        <Options estadoId={+orden?.id_estado_orden_compra} />
-                    </select>
-
-
-                </div>
-
-                <div className='flex justify-around mt-4'>
-                    <ButtonLayout
-                        onClick={handleCancel}
-                        className='bg-red-500 hover:bg-red-600'
-                        type='button'
-                    >
-                        Cancelar
-                    </ButtonLayout>
-                    <ButtonLayout className={"disabled:opacity-50"} type='submit' disabled={orden === localOrden}>
-                        Guardar cambios
-                    </ButtonLayout>
-                </div>
-            </form>
-        </>
-    )
+        <div className='flex justify-around mt-4'>
+          <ButtonLayout
+            onClick={handleCancel}
+            className='bg-red-500 hover:bg-red-600'
+            type='button'
+          >
+            Cancelar
+          </ButtonLayout>
+          <ButtonLayout
+            className={'disabled:opacity-50'}
+            type='submit'
+            disabled={orden === localOrden}
+          >
+            Guardar cambios
+          </ButtonLayout>
+        </div>
+      </form>
+    </>
+  )
 }
--- src/components/ordenes/OrdenCard.jsx
+++ src/components/ordenes/OrdenCard.jsx
@@ -1,28 +1,45 @@
-import { Edit } from "lucide-react"
+import { Edit } from 'lucide-react'
 
 export const OrdenCard = ({ orden, onClick }) => {
+  return (
+    <div
+      key={orden.id_orden_compra}
+      className='bg-white rounded-xl shadow-md border border-gray-200 p-6 transition-transform hover:scale-105 hover:shadow-lg'
+    >
+      <div className='flex justify-between items-center'>
+        <h3 className='text-xl font-bold text-gray-800 mb-2'>
+          Orden #{orden.id_orden_compra}
+        </h3>
+        {onClick && (
+          <button onClick={() => onClick(orden)} className='cursor-pointer'>
+            <Edit className='w-5 h-5 text-blue-600' />
+          </button>
+        )}
+      </div>
 
-    return (
-        <div
-            key={orden.id_orden_compra}
-            className="bg-white rounded-xl shadow-md border border-gray-200 p-6 transition-transform hover:scale-105 hover:shadow-lg"
-        >
-            <div className="flex justify-between items-center">
-                <h3 className="text-xl font-bold text-gray-800 mb-2">Orden #{orden.id_orden_compra}</h3>
-                {onClick && <button onClick={() => onClick(orden)} className="cursor-pointer">
-                    <Edit className='w-5 h-5 text-blue-600' />
-                </button>}
-            </div>
+      <p className='text-gray-700'>
+        <strong>Proveedor:</strong> {orden.proveedorArticulo.proveedor.apellido}{' '}
+        {orden.proveedorArticulo.proveedor.nombre}
+      </p>
 
-            <p className="text-gray-700"><strong>Proveedor:</strong> {orden.proveedorArticulo.proveedor.apellido} {orden.proveedorArticulo.proveedor.nombre}</p>
+      <p className='text-gray-700'>
+        <strong>Artículo:</strong>{' '}
+        {orden.proveedorArticulo.articulo.descripcion}
+      </p>
 
-            <p className="text-gray-700"><strong>Artículo:</strong> {orden.proveedorArticulo.articulo.descripcion}</p>
+      <p className='text-gray-700'>
+        <strong>Fecha estimada de recepción:</strong>{' '}
+        {new Date(orden.fecha_estimada_recepcion).toLocaleDateString()}
+      </p>
 
-            <p className="text-gray-700"><strong>Fecha estimada:</strong> {new Date(orden.fecha_estimada_recepcion).toLocaleDateString()}</p>
+      <p className='text-gray-700'>
+        <strong>Cantidad:</strong> {orden.cantidad}
+      </p>
 
-            <p className="text-gray-700"><strong>Cantidad:</strong> {orden.cantidad}</p>
-
-            <p className="text-gray-700"><strong>Estado:</strong> {orden.estadoOrdenCompra.nombre_eoc.toUpperCase()}</p>
-        </div>
-    )
+      <p className='text-gray-700'>
+        <strong>Estado:</strong>{' '}
+        {orden.estadoOrdenCompra.nombre_eoc.toUpperCase()}
+      </p>
+    </div>
+  )
 }
